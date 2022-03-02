671d2cd9c7c64a83b2c3406ecd0292d2c8b16b80
THOMAS HARDING <hardingt604@gmail.com>
2022-03-02 15:58:12 -0700

    added select tag to index.html with two options and changed data.js ids

--- data.js
+++ data.js
@@ -1,21 +1,16 @@
 export const people = [
     {
-        id: 0,
+        id: 1,
         name: 'Thomas',
         pronouns: 'he/him',
         favorite_place: 'Japan',
         hobbies: ['vidya', 'sleeping'],
-
     },
-
     {
-        id: 1,
+        id: 2,
         name: 'Hailey',
         pronouns: 'she/her',
         favorite_place: 'hotsprings',
         hobbies: ['astrology', 'poetry', 'puzzles'],
-
     },
-
-
 ];
