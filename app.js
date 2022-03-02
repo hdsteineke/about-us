cae94820134e98ec538c9e2fb58def94284d02b1
THOMAS HARDING <hardingt604@gmail.com>
2022-03-02 16:33:09 -0700

    grabbed DOM elements in app.js and created function to fill them with passed index

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 import { people } from './data.js';
 
 const personSelector = document.querySelector('.select');
+const filledName = document.querySelector('#name');
+const filledPronouns = document.querySelector('.pronouns');
+const filledPlace = document.querySelector('.favorite_place');
+const filledHobbies = document.querySelector('.hobbies');
+
+personSelector.addEventListener('change', (e) => {
+    //call renderPerson with index corresponding to the name that just got selected
+});
+
+function renderPerson(index) {
+    filledName.textContent = 'Name: ' + people[index].name;
+    filledPronouns.textContent = 'Pronouns: ' + people[index].pronouns;
+    filledPlace.textContent = 'Favorite Place: ' + people[index].favorite_place;
+
+    for (let hobby of people[index].hobbies) {
+        const li = document.createElement('li');
+        li.textContent = hobby;
+        filledHobbies.appendChild(li);
+    }
+}
