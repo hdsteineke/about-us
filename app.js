efb8bdc6a47e03e74a7472a5749c755fe2006970
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-02 16:33:40 -0800

    working person selector

--- app.js
+++ app.js
@@ -5,12 +5,26 @@ const filledName = document.querySelector('#name');
 const filledPronouns = document.querySelector('.pronouns');
 const filledPlace = document.querySelector('.favorite_place');
 const filledHobbies = document.querySelector('.hobbies');
+const contain = document.querySelector('.contain');
 
-personSelector.addEventListener('change', (e) => {
-    //call renderPerson with index corresponding to the name that just got selected
+personSelector.addEventListener('change', (event) => {
+    filledHobbies.innerHTML = ' ';
+    contain.classList.remove('Hailey', 'Thomas');
+
+    const selected = event.target.value;
+
+    if (selected === 'one' ) {
+        renderPerson(0);
+    } else {
+        renderPerson(1);
+    }
+
+    
 });
 
 function renderPerson(index) {
+    contain.classList.add(`${people[index].name}`);
+
     filledName.textContent = 'Name: ' + people[index].name;
     filledPronouns.textContent = 'Pronouns: ' + people[index].pronouns;
     filledPlace.textContent = 'Favorite Place: ' + people[index].favorite_place;
